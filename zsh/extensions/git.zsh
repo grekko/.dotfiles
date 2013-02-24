5a52921d9f4f8f35a29cda10680db796396a7503
Gregory Igelmund <gregory.igelmund@gmail.de>
2013-02-24 16:14:39 +0100

    added git as plugin

---
+++ zsh/extensions/git.zsh
@@ -0,0 +1,18 @@
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
+}
+
+alias gpp='git pull origin $(current_branch) && git push origin $(current_branch)'
+alias gup='git stash && git pull origin $(current_branch) --rebase && git stash pop'
+alias gp='git push origin $(current_branch)'
+alias gm='git merge'
+alias gd='git diff'
+alias gss='git status -s'
