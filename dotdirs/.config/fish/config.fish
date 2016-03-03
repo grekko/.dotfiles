ebb7ae8f70df21f39d1d38474dc192ac3d831fdb
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-03-03 08:33:21 +0700

    Enable host specific fish config

--- dotdirs/.config/fish/config.fish
+++ dotdirs/.config/fish/config.fish
@@ -1,1 +1,9 @@
-source ~/.dotfiles_env
+set -x DOTFILES_HOME $HOME/.dotfiles
+set -x DOTFILES_HOSTNAME (hostname -s)
+set -x DOTFILES_ENV_PATH $DOTFILES_HOME/fish/environments/$DOTFILES_HOSTNAME
+
+if test -s $DOTFILES_ENV_PATH
+  source $DOTFILES_ENV_PATH
+else
+  echo "Create an environment file to customize your shells env: $DOTFILES_ENV_PATH » Currently running in default mode."
+end
