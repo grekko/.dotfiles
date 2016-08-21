723fde4453694827ec4fabf2d6ec9a5b65ade91a
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-08-21 15:36:11 +0200

    Shell: Allow bash/fish to share some functioniality

--- dotdirs/.config/fish/config.fish
+++ dotdirs/.config/fish/config.fish
@@ -1,23 +1,13 @@
 # vim: set ft=sh :
 set -x DOTFILES_HOME $HOME/.dotfiles
 set -x DOTFILES_HOSTNAME (hostname -s)
-set -x DOTFILES_ENV_PATH $DOTFILES_HOME/fish/environments/$DOTFILES_HOSTNAME
+set -x DOTFILES_HOST_FISH_CONFIG $DOTFILES_HOME/fish/environments/$DOTFILES_HOSTNAME
+set -x DOTFILES_SHELL_CONFIG "$HOME/.config/fish/config.fish"
 
-if test -e $DOTFILES_ENV_PATH
-  source $DOTFILES_ENV_PATH
-end
-
-function up!
-  pushd
-  cd $DOTFILES_HOME
-  git stash
-  git pull origin master
-  git stash pop
-  # Initialize git submodules which may have been added
-  git submodule init
-  git submodule update
-  popd
-  . ~/.config/fish/config.fish
+if test -e $DOTFILES_HOST_FISH_CONFIG
+  source $DOTFILES_HOST_FISH_CONFIG
+else
+  echo "No host specific fish config detected. You can create one at $DOTFILES_HOST_FISH_CONFIG"
 end
 
 # Enable vi mode for fish
--- dotfiles/.bashrc
+++ dotfiles/.bashrc
@@ -1,1 +1,12 @@
+export DOTFILES_HOME="${HOME}/.dotfiles"
+export DOTFILES_HOSTNAME=`hostname -f`
+export DOTFILES_HOST_BASH_CONFIG="${HOME}/.bashrc/machines/${DOTFILES_HOSTNAME}"
+export DOTFILES_SHELL_CONFIG="${HOME}/.bashrc"
+
+if [[ -s $DOTFILES_HOST_BASH_CONFIG ]]; then
+  source $DOTFILES_HOST_BASH_CONFIG
+else
+  echo "No host specific bashrc found. You can create one here: ${DOTFILES_HOST_BASH_CONFIG}"
+fi
+
 [[ -s $HOME/.bashrc.local ]] && source $HOME/.bashrc.local
