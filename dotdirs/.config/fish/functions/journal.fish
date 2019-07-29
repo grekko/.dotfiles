67742f67785cb1504027f998a6da3906c9bcf805
Gregory Igelmund <github.com@grekko.de>
2019-07-29 12:17:15 +0200

    Fish: Remove `journal`-function

--- dotdirs/.config/fish/functions/journal.fish
+++
@@ -1,9 +0,0 @@
-# vim: set ft=sh :
-function journal --description "Opens vim with the current journal file"
-  set -l isodate (date -u +"%Y-%m-%d")
-  set -l filename $HOME/scm/journals/drafts/$isodate.md
-  vim $filename
-end
-
-alias j journal
-
