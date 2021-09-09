a4d23644ca7196a9101f5e746f4a3edfa1ecb2ff
Gregory Igelmund <github.com@grekko.de>
2021-09-09 17:31:07 +0200

    fish/config.fish: Remove obsolete modifications to PATH

--- dotdirs/.config/fish/config.fish
+++ dotdirs/.config/fish/config.fish
@@ -15,6 +15,3 @@ set -x PATH ~/.scripts $PATH
 
 # Load shell aliases
 source "$HOME/.shell_aliases"
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
